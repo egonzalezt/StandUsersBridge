9b7b570054f8fd5d74a82ad9637d217a2d07d39b
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2024-03-05 20:03:54 -0500

    create middleware

---
+++ StandUsersBridge/Middlewares/MessageSendingExceptionHandler.cs
@@ -0,0 +1,41 @@
+﻿namespace StandUsersBridge.Middlewares;
+
+using System.Net;
+using System.Text.Json;
+public class MessageSendingExceptionHandler(RequestDelegate next, ILogger<MessageSendingExceptionHandler> logger)
+{
+    private readonly RequestDelegate _next = next;
+    private readonly ILogger<MessageSendingExceptionHandler> _logger = logger;
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        _logger.LogError(exception, "An error occurred while processing the request.");
+
+        var statusCode = HttpStatusCode.InternalServerError;
+        var message = "Internal server error";
+
+        if (exception is RabbitMQ.Client.Exceptions.BrokerUnreachableException ||
+            exception is RabbitMQ.Client.Exceptions.PossibleAuthenticationFailureException)
+        {
+            statusCode = HttpStatusCode.InternalServerError;
+            message = "Internal server error";
+        }
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)statusCode;
+        var result = JsonSerializer.Serialize(new { message });
+        await context.Response.WriteAsync(result);
+    }
+}
--- StandUsersBridge/Program.cs
+++ StandUsersBridge/Program.cs
@@ -1,4 +1,5 @@
 using StandUsersBridge.Infrastructure.ServiceCollection;
+using StandUsersBridge.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<MessageSendingExceptionHandler>();
 
 app.UseHttpsRedirection();
 
