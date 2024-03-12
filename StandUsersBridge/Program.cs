3c52af5cd98bdbef196a102df0d383c410caaabc
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2024-03-12 00:18:07 -0500

    feat(actions): add docker build

--- StandUsersBridge/Program.cs
+++ StandUsersBridge/Program.cs
@@ -13,15 +13,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<MessageSendingExceptionHandler>();
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
