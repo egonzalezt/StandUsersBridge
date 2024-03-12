067e1999352366254589f8ca2a524477f661128f
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2024-03-12 18:20:05 -0500

    fix(dotnet): add env support

--- StandUsersBridge/Program.cs
+++ StandUsersBridge/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Configuration.AddEnvironmentVariables();
 
 var app = builder.Build();
 
